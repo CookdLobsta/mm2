0c17adffbf7ee5d1ae9026db786ab3bb842f6129
Connor Wilson <connormwilson@gmail.com>
2019-01-26 22:45:49 -0700

    mlab connect

--- client/src/connect.js
+++ client/src/connect.js
@@ -1,7 +1,7 @@
 const mongo = require('mongodb').MongoClient
 
 function doConnect() {
- 	var url = 'mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds155774.mlab.com:55774/marketmendb'
+ 	var url = `mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds155774.mlab.com:55774/marketmendb`
 	mongo.connect(url, (err, db) => {
 		if(err) { 
 			throw err
